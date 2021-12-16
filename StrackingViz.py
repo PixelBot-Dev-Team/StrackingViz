4ac9814bba3b3f0dfba2a05bbf883dc1bada01ba
JustCallMeSimon <no>
2021-12-16 19:46:31 +0100

    Made it accept a File via Drag'n'Drop Made Error Template cause idk

--- StrackingViz.py
+++ StrackingViz.py
@@ -1,5 +1,31 @@
-# Main 
+import sys
+import json
+import time
 
-#May use 1 Class for organization
+def doError(message,id = -1):
+	print(f"""AN ERROR OCCURED!
+Error Message: {message}
+Error ID: {id}
+Closing in 5 Seconds
+	"""
+	)
+	time.sleep(5)
+	exit(id)
 
-#Keep everything in 1 File
+def parsedata(Data):
+	print(Data)
+
+while True:
+	try:
+		File = sys.argv[1] 
+	except IndexError:
+		doError("Drop a Save-Tracking File onto this executable to generate a corresponding Video",1)
+	else:
+		try:
+			with open(File,"r") as JsonFile:			
+				Data = json.load(JsonFile)
+		except json.decoder.JSONDecodeError:
+			pass
+			doError("Json File is corrupted",2)
+		else:
+			parsedata(Data)
